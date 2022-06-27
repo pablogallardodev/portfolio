89f50e5aa745cb01b4ca4d1c9deae2c555023a0e
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-06-27 16:00:27 -0500

    Primer diseno para pruebas

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function App() {
+  return <>
+    <div className='container'>
+      <div>
+        <h1>Hola!!! 👋, yo soy</h1>
+        <h1 className='name'>Pablo Gallardo</h1>
+        <h2 className='title'>Front-end Developer</h2>
+        <button className='cv'>Donwload CV</button>
+        <div className='redes'>
+          <span>GIT</span>
+          <span>INS</span>
+          <span>TWI</span>
+          <span>YOU</span>
+        </div>
+      </div>
+      <div>
+        <h1>img</h1>
+      </div>
+    </div>
+  </>
+}
--- src/Portfolio.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Portfolio = () => {
-  return <>
-    <h1>Portfolio</h1>
-  </>
-}
-
-export default Portfolio
