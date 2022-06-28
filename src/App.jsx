54f04b8a44b4f97f7c4bf326f1156b4e44474c9f
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-06-28 18:14:56 -0500

    add images and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Profile from './picture/profile.png'
+import github from './picture/github.png'
+import instagram from './picture/instagram.png'
+import twitch from './picture/twitch.png'
+import youtube from './picture/youtube.png'
 
 export default function App() {
   return <>
@@ -9,14 +14,14 @@ export default function App() {
         <h2 className='title'>Front-end Developer</h2>
         <button className='cv'>Donwload CV</button>
         <div className='redes'>
-          <span>GIT</span>
-          <span>INS</span>
-          <span>TWI</span>
-          <span>YOU</span>
+          <a href="https://github.com/pablogallardodev" target="blank"><img src={github} alt="Pablo Gallardo" /></a>
+          <a href="https://www.youtube.com/channel/UCS-YoU7f8PztGHBd4OD9RSw" target="blank"><img src={youtube} alt="Pablo Gallardo" /></a>
+          <a href="https://www.instagram.com/pablogallardo.dev" target="blank"><img src={instagram} alt="Pablo Gallardo" /></a>
+          <a href="https://www.twitch.tv/pablogallardodev" target="blank"><img src={twitch} alt="Pablo Gallardo" /></a>
         </div>
       </div>
       <div>
-        <h1>img</h1>
+        <img src={Profile} alt="Pablo Gallardo" />
       </div>
     </div>
   </>
