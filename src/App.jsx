a99353bf45f6a03699392dca3d12c4be92b0fe96
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-06-29 18:27:05 -0500

    Probando animaciones y asignando estructura al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+
 import Profile from './picture/profile.png'
 import github from './picture/github.png'
 import instagram from './picture/instagram.png'
 import twitch from './picture/twitch.png'
 import youtube from './picture/youtube.png'
 
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 export default function App() {
-  return <>
-    <div className='container'>
+  return (
+  <div className='container'>
+    <Navbar />
+
+    <div className='header'>
       <div>
-        <h1>Hola!!! 👋, yo soy</h1>
+        <h1>Hola!!! <span className='hand'>👋</span>, yo soy</h1>
         <h1 className='name'>Pablo Gallardo</h1>
         <h2 className='title'>Front-end Developer</h2>
         <button className='cv'>Donwload CV</button>
@@ -24,5 +30,13 @@ export default function App() {
         <img src={Profile} alt="Pablo Gallardo" />
       </div>
     </div>
-  </>
+    
+    <div className='about-me'>
+      <h1>About me</h1>
+      <p>Ingeniero en Sistemas Computacionales 👨‍🎓, apasionado por ser desarrollador, aprender y aplicar nuevas tecnologías. Actualmente soy desarrollador Front-End 💻 🖱 para una consultora. Un hobby es poder compartir mis conocimientos 📹 📺, para que los demás puedan aprender más. Por último, pero no menos importante soy de México. 🌮 🌮</p>
+    </div>
+
+    <Footer />
+  </div>
+  )
 }
