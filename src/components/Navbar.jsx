4faa379751ed567a11f9b7003c9af6dfa7a0f420
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-07-07 19:40:02 -0500

    Css module y diseño de back

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import logo from "../picture/profile.png"
+import styles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav className='nav app-center'>
+    <nav className={styles.nav}>
       <img src={logo} alt="Logo" width="60" />
       <ul>
-        <li>me</li>
         <li>About me</li>
         <li>Proyectos</li>
         <li>Contacto</li>
