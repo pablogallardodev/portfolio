e3b7ad54fab9a7d7c6e682178c7b8ff8a7d93a68
pablogallardodev <pablito.260197@gmail.com>
2023-12-20 12:58:26 -0600

    (feature): Add notion integration to read projects

--- src/hooks/useNotion.js
+++ src/hooks/useNotion.js
@@ -1,11 +1,29 @@
-import { Client } from '@notionhq/client'
-
-const notion = new Client({ auth: 'secret' })
+import { useState, useEffect } from 'react'
+const { VITE_NOTION_SECRET, VITE_NOTION_DATABASE_ID } = import.meta.env
 
 export const useNotion = async () => {
-  const projects = await notion.databases.query({
-    database_id: 'database_id'
-  })
+  const [projects, setProjects] = useState([])
 
-  console.log(projects.results)
+  useEffect(() => {
+    const headersList = {
+      Authorization: `Bearer ${VITE_NOTION_SECRET}`,
+      'Notion-Version': '2022-06-28',
+      'Content-Type': 'application/json'
+    }
+
+    fetch(
+      `https://api.notion.com/v1/databases/${VITE_NOTION_DATABASE_ID}/query`,
+      {
+        method: 'POST',
+        headers: headersList
+      }
+    ).then((res) => res.json())
+      .then((data) => {
+        console.log('Notion - ', data)
+      })
+
+    setProjects([])
+  }, [])
+
+  return projects
 }
